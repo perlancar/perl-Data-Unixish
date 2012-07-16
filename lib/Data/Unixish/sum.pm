960b76c86d95a8e499571d8ab654b12f3697f431
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2012-07-17 00:10:13 +0700

    Add dux function: sum

---
+++ lib/Data/Unixish/sum.pm
@@ -0,0 +1,36 @@
+package Data::Unixish::sum;
+
+use 5.010;
+use strict;
+use warnings;
+use Log::Any '$log';
+use Scalar::Util 'looks_like_number';
+
+# VERSION
+
+our %SPEC;
+
+$SPEC{sum} = {
+    v => 1.1,
+    summary => 'Sum numbers',
+    args => {
+        in  => {schema=>'any'},
+        out => {schema=>'any'},
+    },
+    tags => [qw/group/],
+};
+sub sum {
+    my %args = @_;
+    my ($in, $out) = ($args{in}, $args{out});
+
+    my $sum = 0;
+    while (my ($index, $item) = each @$in) {
+        $sum += $item if looks_like_number($item);
+    }
+
+    push @$out, $sum;
+    [200, "OK"];
+}
+
+1;
+# ABSTRACT: Sum numbers
