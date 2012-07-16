c21d3e5b2f2e45a932c370f384e0b9f4479e58fb
Steven Haryanto (on Asus EEEPC) <stevenharyanto@gmail.com>
2012-07-16 17:22:01 +0700

    Make 'dux FUNC --help' work (I18N::LANG classes needed too)

---
+++ lib/Perinci/CmdLine/dux/I18N/en.pm
@@ -0,0 +1,16 @@
+package Perinci::CmdLine::dux::I18N::en;
+use parent qw(Perinci::CmdLine::dux::I18N Perinci::CmdLine::I18N::en);
+
+use Locale::Maketext::Lexicon::Gettext;
+our %Lexicon = %{ Locale::Maketext::Lexicon::Gettext->parse(<DATA>) };
+
+# VERSION
+
+#use Data::Dump; dd \%Lexicon;
+
+1;
+# ABSTRACT: English translation for Perinci::CmdLine::dux
+__DATA__
+
+msgid  ""
+msgstr ""
---
+++ lib/Perinci/CmdLine/dux/I18N/id.pm
@@ -0,0 +1,16 @@
+package Perinci::CmdLine::dux::I18N::id;
+use parent qw(Perinci::CmdLine::dux::I18N Perinci::CmdLine::I18N::id);
+
+use Locale::Maketext::Lexicon::Gettext;
+our %Lexicon = %{ Locale::Maketext::Lexicon::Gettext->parse(<DATA>) };
+
+# VERSION
+
+#use Data::Dump; dd \%Lexicon;
+
+1;
+# ABSTRACT: Indonesian translation for Perinci::CmdLine::dux
+__DATA__
+
+msgid  ""
+msgstr ""
