7ea66b98f9c1b0606de112201be2e026edad12fa
Steven Haryanto (on Asus Zenbook) <stevenharyanto@gmail.com>
2012-07-23 22:26:11 +0700

    Add dux func: shuf

---
+++ lib/Data/Unixish/shuf.pm
@@ -0,0 +1,37 @@
+package Data::Unixish::shuf;
+
+use 5.010;
+use strict;
+use warnings;
+use Log::Any '$log';
+
+use List::Util qw(shuffle);
+
+# VERSION
+
+our %SPEC;
+
+$SPEC{shuf} = {
+    v => 1.1,
+    summary => 'Shuffle items',
+    args => {
+        in  => {schema=>'any'},
+        out => {schema=>'any'},
+    },
+    tags => [qw/ordering/],
+};
+sub shuf {
+    my %args = @_;
+    my ($in, $out) = ($args{in}, $args{out});
+
+    my @tmp;
+    while (my ($index, $item) = each @$in) {
+        push @tmp, $item;
+    }
+
+    push @$out, $_ for shuffle @tmp;
+    [200, "OK"];
+}
+
+1;
+# ABSTRACT: Shuffle items
