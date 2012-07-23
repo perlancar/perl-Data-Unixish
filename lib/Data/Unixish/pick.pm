cc6265a41c3f93e0c1ca19d8fa8afe6b463d9109
Steven Haryanto (on Asus Zenbook) <stevenharyanto@gmail.com>
2012-07-23 22:17:22 +0700

    Add dux func: pick

---
+++ lib/Data/Unixish/pick.pm
@@ -0,0 +1,48 @@
+package Data::Unixish::pick;
+
+use 5.010;
+use strict;
+use warnings;
+use Log::Any '$log';
+
+# VERSION
+
+our %SPEC;
+
+$SPEC{pick} = {
+    v => 1.1,
+    summary => 'Pick one or more random items',
+    args => {
+        in  => {schema=>'any'},
+        out => {schema=>'any'},
+        items => {
+            summary => 'Number of items to pick',
+            schema=>['int*' => {default=>1}],
+            tags => ['main'],
+            cmdline_aliases => { n=>{} },
+        },
+    },
+    tags => [qw/filtering/],
+};
+sub pick {
+    my %args = @_;
+    my ($in, $out) = ($args{in}, $args{out});
+    my $n = $args{items} // 1;
+
+    my @picked;
+    while (my ($index, $item) = each @$in) {
+        if (@picked < $n) {
+            push @picked, $item;
+            my ($r1, $r2) = (rand(@picked), rand(@picked));
+            ($picked[$r1], $picked[$r2]) = ($picked[$r2], $picked[$r1]);
+        } else {
+            rand($.) <= $n and $picked[rand(@picked)] = $item;
+        }
+    }
+
+    push @$out, $_ for @picked;
+    [200, "OK"];
+}
+
+1;
+# ABSTRACT: Pick one or more random items
