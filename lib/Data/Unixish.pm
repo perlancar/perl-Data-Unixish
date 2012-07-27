4a333ce7deb96ce510efa291185bbb5917928986
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2012-07-27 18:57:39 +0700

    Add main module (empty for now)

---
+++ lib/Data/Unixish.pm
@@ -0,0 +1,22 @@
+package Data::Unixish;
+
+use 5.010;
+use strict;
+use warnings;
+
+# VERSION
+
+1;
+# ABSTRACT: Implementation for Unixish, a data transformation framework
+
+=head1 DESCRIPTION
+
+This distribution implements L<Unixish>, a data transformation framework
+inspired by Unix toolbox philosophy.
+
+
+=head1 SEE ALSO
+
+L<Unixish>
+
+=cut
