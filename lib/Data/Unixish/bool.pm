d939745d1148d13d8681e8372b56cd5d9bc0cfab
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2013-04-05 16:55:09 +0700

    Add func: bool (format bool)

---
+++ lib/Data/Unixish/bool.pm
@@ -0,0 +1,129 @@
+package Data::Unixish::bool;
+
+use 5.010;
+use strict;
+use syntax 'each_on_array'; # to support perl < 5.12
+use utf8;
+use warnings;
+use Log::Any '$log';
+
+# VERSION
+
+our %SPEC;
+
+sub is_true {
+    my ($val, $notion) = @_;
+
+    if ($notion eq 'n1') {
+        return undef unless defined($val);
+        return 0 if ref($val) eq 'ARRAY' && !@$val;
+        return 0 if ref($val) eq 'HASH'  && !keys(%$val);
+        return $val ? 1:0;
+    } else {
+        # perl
+        return undef unless defined($val);
+        return $val ? 1:0;
+    }
+}
+
+my %styles = (
+    one_zero          => ['1', '0'],
+    t_f               => ['t', 'f'],
+    true_false        => ['true', 'false'],
+    y_n               => ['y', 'n'],
+    Y_N               => ['Y', 'N'],
+    yes_no            => ['yes', 'no'],
+    v_X               => ['v', 'X'],
+    check             => ['✓', ' ', 'uses Unicode'],
+    check_cross       => ['✓', '✕', 'uses Unicode'],
+    heavy_check_cross => ['✔', '✘', 'uses Unicode'],
+    dot               => ['●', ' ', 'uses Unicode'],
+    dot_cross         => ['●', '✘', 'uses Unicode'],
+
+);
+
+$SPEC{bool} = {
+    v => 1.1,
+    summary => 'Format boolean',
+    description => <<'_',
+
+_
+    args => {
+        in  => {schema=>'any'},
+        out => {schema=>'any'},
+        style => {
+            schema=>[str => in=>[keys %styles], default=>'one_zero'],
+            description => "Available styles:\n\n".
+                join("", map {" * $_" . ($styles{$_}[2] ? " ($styles{$_}[2])":"").": $styles{$_}[1] $styles{$_}[0]\n"} sort keys %styles),
+            cmdline_aliases => { s=>{} },
+        },
+        true_char => {
+            summary => 'Instead of style, you can also specify character for true value',
+            schema=>['str*'],
+            cmdline_aliases => { t => {} },
+        },
+        false_char => {
+            summary => 'Instead of style, you can also specify character for true value',
+            schema=>['str*'],
+            cmdline_aliases => { f => {} },
+        },
+        notion => {
+            summary => 'What notion to use to determine true/false',
+            schema => [str => in=>[qw/perl n1/], default => 'perl'],
+            description => <<'_',
+
+`perl` uses Perl notion.
+
+`n1` (for lack of better name) is just like Perl notion, but empty array and
+empty hash is considered false.
+
+TODO: add Ruby, Python, PHP, JavaScript, etc notion.
+
+_
+        },
+        # XXX: flag to ignore references
+    },
+    tags => [qw/format/],
+};
+sub bool {
+    my %args = @_;
+    my ($in, $out) = ($args{in}, $args{out});
+    my $notion = $args{notion} // 'perl';
+    my $style  = $args{style}  // 'one_zero';
+    $style = 'one_zero' if !$styles{$style};
+
+    my $tc = $args{true_char}  // $styles{$style}[0];
+    my $fc = $args{false_char} // $styles{$style}[1];
+
+    while (my ($index, $item) = each @$in) {
+        my $t = is_true($item, $notion);
+        $item = $t ? $tc : defined($t) ? $fc : undef;
+        push @$out, $item;
+    }
+
+    [200, "OK"];
+}
+
+1;
+# ABSTRACT: Format bool
+
+=head1 SYNOPSIS
+
+In Perl:
+
+ use Data::Unixish::bool;
+ my $in  = [0, "one", 2, ""];
+ my $out = [];
+ Data::Unixish::bool(in=>$in, out=>$out, style=>"check_cross");
+ # $out = ["✕","✓","✓","✕"]
+
+In command line:
+
+ % echo -e "0\none\n2\n\n" | dux bool -s y_n --format=text-simple
+ n
+ y
+ y
+ n
+
+=cut
+
