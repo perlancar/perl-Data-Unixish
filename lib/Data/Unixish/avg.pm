0e3909eb50318995ac3729d0e0543f009c9ca407
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2013-04-11 16:10:52 +0700

    Add func: avg

---
+++ lib/Data/Unixish/avg.pm
@@ -0,0 +1,57 @@
+package Data::Unixish::avg;
+
+use 5.010;
+use strict;
+use syntax 'each_on_array'; # to support perl < 5.12
+use warnings;
+use Log::Any '$log';
+use Scalar::Util 'looks_like_number';
+
+# VERSION
+
+our %SPEC;
+
+$SPEC{avg} = {
+    v => 1.1,
+    summary => 'Average numbers',
+    args => {
+        in  => {schema=>'any'},
+        out => {schema=>'any'},
+    },
+    tags => [qw/group/],
+};
+sub avg {
+    my %args = @_;
+    my ($in, $out) = ($args{in}, $args{out});
+
+    my $sum = 0;
+    my $n = 0;
+    while (my ($index, $item) = each @$in) {
+        $n++;
+        $sum += $item if looks_like_number($item);
+    }
+
+    my $avg = $n ? $sum/$n : 0;
+
+    push @$out, $avg;
+    [200, "OK"];
+}
+
+1;
+# ABSTRACT: Average numbers
+
+=head1 SYNOPSIS
+
+In Perl:
+
+ use Data::Unixish::List qw(dux);
+ my $avg = dux('avg', 1, 2, 3, 4, 5); # => 3
+
+In command line:
+
+ % seq 0 100 | dux avg
+ .----.
+ | 50 |
+ '----'
+
+=cut
