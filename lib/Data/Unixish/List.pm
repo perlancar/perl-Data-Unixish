06679e1e18d91fe134917167c1ff228362ac10ac
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2013-05-18 01:14:14 +0700

    Add functions aduxa, aduxl, lduxa, lduxl

--- lib/Data/Unixish/List.pm
+++ lib/Data/Unixish/List.pm
@@ -12,12 +12,15 @@ use SHARYANTO::Package::Util qw(package_exists);
 
 require Exporter;
 our @ISA       = qw(Exporter);
-our @EXPORT_OK = qw(dux);
+our @EXPORT_OK = qw(dux aduxa aduxl lduxa lduxl);
 
-sub dux {
+sub _dux {
+    my $accepts = shift;
+    my $returns = shift;
+
     my $f = shift;
     my $out = [];
-    my %args = (in => \@_, out => $out);
+    my %args = (in => $accepts eq 'l' ? \@_ : $_[0], out => $out);
     if (ref($f) eq 'ARRAY') {
         $args{$_} = $f->[1]{$_} for keys %{$f->[1]};
         $f = $f->[0];
@@ -34,29 +37,41 @@ sub dux {
     die "Dux function $fn failed: $res->[0] - $res->[1]"
         unless $res->[0] == 200;
 
-    if (wantarray) {
-        return @$out;
+    if ($returns eq 'l') {
+        if (wantarray) {
+            return @$out;
+        } else {
+            return $out->[0];
+        }
     } else {
-        return $out->[0];
+        return $out;
     }
 }
 
+sub dux   { _dux('l', 'l', @_) }
+sub aduxa { _dux('a', 'a', @_) }
+sub aduxl { _dux('a', 'l', @_) }
+sub lduxa { _dux('l', 'a', @_) }
+sub lduxl { _dux('l', 'l', @_) }
+
 1;
-# ABSTRACT: Apply dux function to a list (and return the result as a list)
+# ABSTRACT: Apply dux function to a list/array (and return result as list/array)
 
 =head1 SYNOPSIS
 
- use Data::Unixish::List qw(dux);
+ # prefix 'a'/'l' determines whether function accepts arrayref or list.
+ # suffix 'a'/'l' determines whether function returns arrayref or list.
+ use Data::Unixish::List qw(aduxa aduxl lduxa lduxl);
 
  # no dux function arguments
- my @res = dux('sort', 3, 7, 1, 2); # => (1, 2, 3, 7)
+ my @res = lduxl('sort', 3, 7, 1, 2); # => (1, 2, 3, 7)
 
  # specify dux function arguments
- my @res = dux([lpad => {width=>3, char=>'0'}], 3, 7, 1); # => ('003', '007', '001')
+ my $res = aduxa([lpad => {width=>3, char=>'0'}], [3, 7, 1]); # => ['003', '007', '001']
 
  # only retrieve the first row
  my @nums = (1, 2, 3, 4, "a", 5);
- my $sum  = dux(sum => @nums); # => 15
+ my $sum  = aduxl(sum => \@nums); # => 15
 
 
 =head1 DESCRIPTION
@@ -64,7 +79,13 @@ sub dux {
 
 =head1 FUNCTIONS
 
-=head2 dux($func, @data) => LIST (OR SCALAR)
+=head2 aduxa($func, \@data) => ARRAYREF
+
+=head2 aduxl($func, @data) => ARRAYREF
+
+=head2 lduxa($func, \@data) => LIST (OR SCALAR)
+
+=head2 lduxl($func, @data) => LIST (OR SCALAR)
 
 Apply dux function C<$func> to C<@data>. Return the result list. If called in
 scalar context, return the first row of result list.
@@ -74,14 +95,6 @@ C<Data::Unixish::> prefix) or a 2-element array like C<[$fname, \%args]> where
 the first element is the dux function name and the second element contains the
 arguments for the function. If you do not need to pass any arguments/options to
 the dux function, you can use the simpler string version.
-
-
-=head1 TODO
-
-Rename dux to lduxl (accepts list, returns list). But keep dux as alias to lduxl
-for backward compatibility.
-
-Create aduxl, lduxa, aduxa (accepts arrayref/list, returns arrayref/list).
 
 
 =head1 SEE ALSO
