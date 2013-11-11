a184d77006975425e25cdaa015f587584cbc94d4
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2013-11-11 11:51:04 +0700

    Revert "Workaround to avoid undef warning on 'indent' dux func"
    This reverts commit 37908507e51973b2672422e64a9843b98440fa42.

--- lib/Data/Unixish/indent.pm
+++ lib/Data/Unixish/indent.pm
@@ -44,10 +44,6 @@ sub indent {
             $item =~ s/^/$indent/mg;
         }
 
-        # weird, keeps getting undef warning here on one of my scripts, but
-        # there is actually no undef value when $out/$item is dumped
-        no warnings;
-
         push @$out, $item;
     }
 
