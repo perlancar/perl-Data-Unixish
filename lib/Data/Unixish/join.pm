974926890a0c3da71f75705effad3ef2b4c93824
perlancar (on pc-home) <perlancar@gmail.com>
2019-10-26 07:56:00 +0700

    Add dux function: join

---
+++ lib/Data/Unixish/join.pm
@@ -0,0 +1,62 @@
+package Data::Unixish::join;
+
+# DATE
+# DIST
+# VERSION
+
+use 5.010001;
+use strict;
+use syntax 'each_on_array'; # to support perl < 5.12
+use warnings;
+#use Log::Any '$log';
+
+use Data::Unixish::Util qw(%common_args);
+
+our %SPEC;
+
+$SPEC{join} = {
+    v => 1.1,
+    summary => 'Join elements of array into string',
+    description => <<'_',
+
+_
+    args => {
+        %common_args,
+        string => {
+            summary => 'String to join elements with',
+            schema  => 'str*',
+            default => '',
+            pos     => 0,
+        },
+    },
+    tags => [qw/text datatype-in:array itemfunc/],
+};
+sub join {
+    my %args = @_;
+    my ($in, $out) = ($args{in}, $args{out});
+
+    while (my ($index, $item) = each @$in) {
+        push @$out, ref $item eq 'ARRAY' ? CORE::join($args{string}//'', @$item) : $item;
+    }
+
+    [200, "OK"];
+}
+
+sub _join_item {
+    my ($item, $args) = @_;
+
+    ref $item eq 'ARRAY' ? CORE::join($args->{string}//'', @$item) : $item;
+}
+
+1;
+# ABSTRACT:
+
+=head1 SYNOPSIS
+
+In Perl:
+
+ use Data::Unixish qw(lduxl);
+ @res = lduxl([join => {string=>', '}], ["a","b","c"], ["d","e"], "f,g");
+ # => ("a, b, c", "d, e", "f,g")
+
+=cut
